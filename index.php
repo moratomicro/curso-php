be8a83b7ada538a1ba2bff290c6595924b7fb3ce
moratomicro <moratomicro@yahoo.com.br>
2021-11-04 14:17:52 -0300

    Criado form para enviar dados para script.php

--- index.php
+++ index.php
@@ -1,3 +1,18 @@
-<?php
-    
-?>
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Curso Basico de PHP</title>
+</head>
+<body>
+    <p>FORMULÁRIO PARA INSCRIÇÃO DE COMPETIDORES</p>
+
+    <form action="script.php" method="post">
+        <p>Nome: <input type="text" name="nome" /></p>
+        <p>Idade: <input type="text" name="idade" /></p>
+        <p><input type="submit" value="Enviar dados do competidor"/></p>
+    </form>
+</body>
+</html>
